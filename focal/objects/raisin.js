e33fbd8427c91dd4ffebc7e4902ca9f0e719d6a9
GardRyan <res.gardner@gmail.com>
2024-01-11 09:17:52 -0500

    eslint code, true cleanup

--- focal/objects/raisin.js
+++ focal/objects/raisin.js
@@ -3,38 +3,38 @@
 const raisinAlarm = function(cookie) {
   for (const item of cookie) {
     if (item === "🍇")
-    return "Raisin alert!";
+      return "Raisin alert!";
   }
   return "All good!";
-}
+};
 
 
-  console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
-  console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
-  console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
 
 
 // Second problem
 
-const raisinAlarmArray = function (cookies) {
+const raisinAlarmArray = function(cookies) {
   const responses = [];
 
   for (const cookie of cookies) { //Cookies == main array, cookie == sub array
-     if (cookie.includes("🍇")) { //values of childarray
+    if (cookie.includes("🍇")) { //values of childarray
       responses.push("Raisin alert!"); //value we don't want
     } else {
-    responses.push("All good!"); //return all good if no value
+      responses.push("All good!"); //return all good if no value
     }
   }
   return responses;
 };
 
-  console.log(
-    raisinAlarmArray([
-      ["🍫", "🍫", "🍇", "🍫"],
-      ["🍫", "🍇", "🍫", "🍫", "🍇"],
-      ["🍫", "🍫", "🍫"],
-    ])
-  ); 
+console.log(
+  raisinAlarmArray([
+    ["🍫", "🍫", "🍇", "🍫"],
+    ["🍫", "🍇", "🍫", "🍫", "🍇"],
+    ["🍫", "🍫", "🍫"],
+  ])
+);
 
-  //Expected: ["Raisin alert!", "Raisin alert!", "All good!"]
+//Expected: ["Raisin alert!", "Raisin alert!", "All good!"]
